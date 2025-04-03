e0dd0fea5c093082f95d2fed6ef4d4e5ef82eccf
ZF <freeman.zcl@gmail.com>
2025-04-03 10:37:21 +0100

    file ordering

--- 3_bh-files_formatting/1c_move_onsdur_files.sh
+++ 3_bh-files_formatting/1c_move_onsdur_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Source and destination folders
-source_folder="/data/project/STAR/STAR_preprocessed/L_OnsDur/"
-destination_folder="/data/project/STAR/STAR_preprocessed/L_fmriprep/data/derivatives/"
+source_folder="/data/project/STAR/STAR_preprocessed/L2_fmriprep/data/L2_onsdur_20250401/"
+destination_folder="/data/project/STAR/STAR_preprocessed/L2_fmriprep/data/derivatives/"
 
 # Loop through all .mat files in the source folder
 for file in "${source_folder}"*.mat; do
@@ -12,7 +12,7 @@ for file in "${source_folder}"*.mat; do
 
     if [[ -n "$sub_id" ]]; then
         # Construct the destination directory
-        destination_dir="${destination_folder}${sub_id}/ses-mri01/func/"
+        destination_dir="${destination_folder}${sub_id}/ses-mri02/func/"
 
         # Ensure the destination directory exists, create it if necessary
         mkdir -p "$destination_dir"
